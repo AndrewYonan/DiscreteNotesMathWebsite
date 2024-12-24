8ac6119d09af90c7866ab78691e307156f599953
= <andrew.yonan01@gmail.com>
2024-12-23 19:11:22 -0700

    logic section update

---
+++ src/main.js
@@ -0,0 +1,51 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    set_exercise_dropdown_and_answer_ids();
+    set_exercise_numbers();
+});
+
+
+
+
+function set_exercise_numbers() {
+    const elements = document.getElementsByClassName("exercise");
+    const exercise_arr = Array.from(elements);
+    exercise_arr.forEach((elem, idx) => {
+        const h2 = elem.querySelector("h2");
+        h2.innerHTML = `Exercise ${idx + 1}`;
+    });
+}
+
+
+
+function toggle_solution(solution_id, button_elem) {
+    const answer_elem = document.getElementById(solution_id);
+    if (answer_elem.style.display === 'none' || !answer_elem.style.display) {
+        button_elem.innerHTML = "Hide Solution"
+        answer_elem.style.display = 'block';
+    } else {
+        answer_elem.style.display = 'none';
+        button_elem.innerHTML = "Show Solution"
+    }
+}
+
+
+
+function set_exercise_dropdown_and_answer_ids() {
+
+    const elements = document.getElementsByClassName("exercise");
+    const exercise_arr = Array.from(elements);
+
+    exercise_arr.forEach((elem, idx) => {
+
+        const dropdown_button = elem.querySelector("button");
+        const solution_div = elem.querySelector(".solution");
+        const solution_id = `solution-${idx}`;
+        const on_click_method_call = `toggle_solution('${solution_id}', this)`;
+
+        dropdown_button.innerHTML = "Show Solution";
+        solution_div.setAttribute("id", solution_id)
+        dropdown_button.setAttribute("onclick", on_click_method_call);
+    })
+
+}
--- src/mathjax-config.js
+++ src/mathjax-config.js
@@ -2,3 +2,9 @@ window.MathJax = {
     loader: {load: ['[tex]/color']},
     tex: {packages: {'[+]': ['color']}}
 };
+MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      processEscapes: true
+    }
+  });
